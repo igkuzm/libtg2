76e20c9a50e149655bc38b4a02cca4c7f195bcbe
Igor V. Sementsov <ig.kuzm@gmail.com>
2025-08-31 00:04:49 +1100

    xcode_project

---
+++ config.h
@@ -0,0 +1,9 @@
+#define TELEGRAM_LAYER 185
+
+#undef MAKE_TEST
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "LibTG"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION 2.0
